05f1fbb78cd1fe690961761804ab78927bc6e014
Tiphaine Viard <tiphaine.viard@gmail.com>
2015-11-16 19:11:13 +0100

    Initial commit

---
+++ CliqueMaster.py
@@ -0,0 +1,144 @@
+# -*-coding:utf8*-
+import sys
+import random
+from collections import deque
+from Clique import Clique
+
+
+class CliqueMaster:
+
+    def __init__(self):
+        self._S = deque()
+        self._S_set = set()
+        self._R = set()
+        self._times = dict()
+        self._nodes = dict()
+
+    def addClique(self, c):
+        """ Adds a clique to S,
+        checking beforehand that this clique is not already present in S. """
+        if c not in self._S_set:
+            self._S.appendleft(c)
+            #self._S.append(c)
+            self._S_set.add(c)
+
+    def getClique(self):
+        c = self._S.pop()
+        sys.stderr.write("\nGetting clique " + str(c) + "\n")
+        return c
+
+    def getDeltaClique(self, clique, delta):
+        """ Explores one path in the configuration space and returns a maximal clique """
+        succ = set([clique])
+
+        # Remove clique from S so it will not be picked again
+        self._S.remove(clique)
+
+        while len(succ) > 0:
+            c = succ.pop()
+
+            # Grow time on the right side
+            td = c.getTd(self._times, delta)
+            if c._te != td + delta:
+                c_add = Clique((c._X, (c._tb, td + delta)), c._candidates)
+                succ.add(c_add)
+
+            # Grow time on the left side
+            tp = c.getTp(self._times, delta)
+            if c._tb != tp - delta:
+                c_add = Clique((c._X, (tp - delta, c._te)), c._candidates)
+                succ.add(c_add)
+
+            # Grow node set
+            candidates = c.getAdjacentNodes(self._times, self._nodes, delta)
+
+            for node in candidates:
+                if c.isClique(self._times, node, delta):
+                    Xnew = set(c._X).union([node])
+                    c_add = Clique(
+                        (frozenset(Xnew), (c._tb, c._te)), c._candidates)
+                    succ.add(c_add)
+
+            # Choose one path uniformly at random
+            if(len(succ) > 0):
+                next_c = random.choice(list(succ))
+                succ = set([next_c])
+
+        return c
+
+    def getDeltaCliques(self, delta):
+        """ Returns a set of maximal cliques. """
+        iternum = 0
+        maxsize = 0
+        maxdur = 0
+
+        while len(self._S) != 0:
+            iternum += 1
+            sys.stderr.write("S:" + str(len(self._S)) + "\n")
+            sys.stderr.write("T " + str(iternum) + " " + str(len(self._R)) + "\n")
+            c = self.getClique()
+            is_max = True
+
+            # Grow time on the right side
+            td = c.getTd(self._times, delta)
+            if c._te != td + delta:
+                c_add = Clique((c._X, (c._tb, td + delta)), c._candidates)
+                self.addClique(c_add)
+                sys.stderr.write(
+                    "Adding " +
+                    str(c_add) +
+                    " (time delta extension)\n")
+                is_max = False
+            else:
+                sys.stderr.write(str(c) + " cannot grow on the right side\n")
+
+            # Grow time on the left side
+            tp = c.getTp(self._times, delta)
+            if c._tb != tp - delta:
+                c_add = Clique((c._X, (tp - delta, c._te)), c._candidates)
+                self.addClique(c_add)
+                sys.stderr.write(
+                    "Adding " +
+                    str(c_add) +
+                    " (left time delta extension)\n")
+                is_max = False
+            else:
+                sys.stderr.write(str(c) + " cannot grow on the left side\n")
+
+            # Grow node set
+            candidates = c.getAdjacentNodes(self._times, self._nodes, delta)
+            sys.stderr.write("    Candidates : %s.\n" % (str(candidates)))
+
+            for node in candidates:
+                if c.isClique(self._times, node, delta):
+                    Xnew = set(c._X).union([node])
+                    sys.stderr.write(str(candidates) +
+                                     " VS " +
+                                     str(c._candidates) +
+                                     "\n")
+                    c_add = Clique(
+                        (frozenset(Xnew), (c._tb, c._te)), c._candidates)
+                    self.addClique(c_add)
+                    sys.stderr.write(
+                        "Adding " +
+                        str(c_add) +
+                        " (node extension)\n")
+                    is_max = False
+
+            if is_max:
+                sys.stderr.write(str(c) + " is maximal\n")
+                maxsize = max(maxsize, len(c._X))
+                maxdur = max(maxdur, c._te - c._tb)
+                sys.stderr.write("M " + str(iternum) + " " + str(maxsize) + " " + str(maxdur) + "\n")
+                self._R.add(c)
+        return self._R
+
+    def printCliques(self):
+        for c in self._R:
+            sys.stdout.write(str(c) + "\n")
+
+    def __str__(self):
+        msg = ""
+        for c in self._R:
+            msg += str(c) + "\n"
+        return msg
---
+++ main.py
@@ -0,0 +1,127 @@
+from CliqueMaster import CliqueMaster
+from Clique import Clique
+import matplotlib.pyplot as plt
+import sys
+import random
+
+# Initiate
+Cm = CliqueMaster()
+times = dict()
+nodes = dict()
+nb_lines = 0
+resurrect = False
+
+# Read arguments from command line
+if len(sys.argv) == 2 and "resurrect" not in sys.argv[1]:
+    delta = int(sys.argv[1])
+elif len(sys.argv) == 3:
+    if "resurrect" in sys.argv[2]:
+        resurrect = True
+        delta = int(sys.argv[1])
+else:
+    sys.stderr.write(
+        "Usage: cat <stream> | python main.py <delta> [--resurrect=<err_file>]\
+        \n\n")
+    sys.stderr.write("  --resurrect: Recover previous instance by providing\
+    an stderr output \
+    previous instance.\n\n")
+    sys.exit(1)
+
+# Read stream
+for line in sys.stdin:
+    contents = line.split(" ")
+    t = int(contents[0])
+    u = contents[1].strip()
+    v = contents[2].strip()
+
+    link = frozenset([u, v])
+    time = (t, t)
+
+    # This a new instance
+    if not resurrect:
+        Cm.addClique(Clique((link, time), set([])))
+
+    # Populate data structures
+    if link not in times:
+        times[link] = []
+    times[link].append(t)
+
+    if u not in nodes:
+        nodes[u] = set()
+
+    if v not in nodes:
+        nodes[v] = set()
+
+    nodes[u].add(v)
+    nodes[v].add(u)
+    nb_lines = nb_lines + 1
+Cm._times = times
+Cm._nodes = nodes
+# sys.stderr.write("Processed " + str(nb_lines) + " from stdin\n")
+
+# Populate CliqueManager from err file
+if resurrect:
+    with open(sys.argv[3]) as error_file:
+        for line in error_file:
+            if line[0] == 'G':
+                # Remove from S
+                clique = line.split(" ")[2] + " " + line.split(" ")[3]
+                nodes = line.split(" ")[2].split(",")
+                duration = line.split(" ")[3].split("(")[0].split(",")
+                c = Clique(
+                    (frozenset(nodes), (int(
+                        duration[0]), int(
+                        duration[1]))))
+
+                if c in Cm._S:
+                    Cm._S.remove(c)
+
+            elif line[0] == 'A':
+                # Add to S and S_set
+                clique = line.split(" ")[1] + " " + line.split(" ")[2]
+                nodes = line.split(" ")[1].split(",")
+                duration = line.split(" ")[2].split(",")
+                c = Clique(
+                    (frozenset(nodes), (int(
+                        duration[0]), int(
+                        duration[1]))))
+
+                Cm._S.append(c)
+                Cm._S_set.add(c)
+
+            elif "maximal" in line:
+                # Add to R
+                clique = line.split(" ")[0] + " " + line.split(" ")[1]
+                nodes = line.split(" ")[0].split(",")
+                duration = line.split(" ")[1].split(",")
+                c = Clique(
+                    (frozenset(nodes), (int(
+                        duration[0]), int(
+                        duration[1]))))
+                Cm._R.add(c)
+
+            # Write original err line to be consistent
+            sys.stderr.write(line)
+
+# (Re)start execution
+# R = Cm.getDeltaCliques(delta)
+# sys.stdout.write("# delta = %d %d\n" % (delta))
+# Cm.printCliques()
+
+# Get one clique (repeat until ctrl+c ? Parallelize ?)
+counts = {}
+plt.ion()
+plt.show()
+
+while len(Cm._S) > 0:    
+    r_clique = random.choice(Cm._S)
+    c = Cm.getDeltaClique(r_clique, delta)
+    val = len(c._X) 
+    try:
+        counts[val] += 1
+    except KeyError:
+        counts[val] = 1
+    plt.scatter(val, counts[val])
+    plt.draw()
+    # time.sleep(0.01)
+    print(c)
